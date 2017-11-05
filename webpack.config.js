be86b0eb31b4a77de24c8886b20a7d537604394e
Sacha Morgese <sachamorgese@gmail.com>
2017-11-05 19:34:16 +0900

    First commit, basic app done.

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,19 @@ module.exports = {
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: './bundle.js'
   },
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
